b45a2ae92db63c888bf852c286eceef281d06fea
AlesjaScha <alesja310780@gmail.com>
2022-11-17 21:13:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {data} from './data';
 import './App.css';
+import photo from './photo.jpeg';
 
 function App() {
+
+const [room,setRooms]=useState(0);
+const{id,image}=data[room];
+ 
+const previousRooms=()=>{
+  setRooms((room =>{
+    room--;
+    if(room<0){
+      return data.length-1;
+    }
+    return room;
+  }))
+}
+const nextRooms =()=>{
+  setRooms((room=>{
+    room++;
+    if(room>data.length-1){
+      room=0;
+    }
+    return room;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='heading'>
+          <h1>Traum Villa in der Schweiz </h1>
+          </div>
+
+          <div className='heading'>
+        <img src={photo}width="1000px"alt="foto" />
+    </div> 
+
+        <div className='heading'>
+             <p>13,5 Zimmer/990 m2 Wohnfläche/2022 Baujahr/Preis:auf an Anfrage</p>
+        </div>
+
+        <div className='heading'>
+            <h3>Die historische Villa und der Annex-Neubau sind je mit eigenen Wohn-Essbereichen , Küchen und Privaträumen mit Ankleiden und Bädern en suite konzipiert. Insgesamt werden etwa 13 Zimmer und ca. 990 m² Wohnfläche zu diesem exklusiven Domizil gehören. Die reichlichen Nebenräume bieten zudem Platz z.B. für eine Einliegerwohnung, ein Heimkino, Fitness- oder Partyraum.Grosse Terrassen beeindrucken mit sagenhafter Sicht über den See bis tief in die Schweizer Bergwelt – eine bezaubernde Panoramalage im steuergünstigen Kanton Schwyz, ruhig und doch in guter Reichweite der Städte Zug, Luzern und Zürich. Historischer Charme und modernster Luxus vereinigen sich hier zu einem einzigartigen Anwesen für Menschen mit höchsten Ansprüchen.</h3>
+        </div>
+    
+
+  <div className='heading'>
+       <h1>{id} </h1>
+  </div>
+
+
+  <div className='heading'>
+  <button onClick={previousRooms}>
+      <img src="https://img.icons8.com/pastel-glyph/512/back.png"width="60px"alt="icon"/>
+  </button>
+
+      <img src={image}width="800px" alt="haus"/>
+    <button onClick = {nextRooms}>
+        <img src="https://img.icons8.com/ios-filled/512/forward.png"width="40px" alt="icon"/>
+    </button>
+  </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const data =[
+
+        {
+            id:1,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-004.jpg/fcc0097ffb365cf08b311b70db2f0ddd.jpg"
+        },
+
+        {
+            id:2, 
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-wohnen.jpg/32d79d056ec0558f68af1b1ddad5af36.jpg",
+            
+        },
+
+        {
+            id:3,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-bibliothek.jpg/70e5c8732a82874e6b246ee541e83e3b.jpg",
+            
+        },
+
+        {
+            id:4,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-kueche.jpg/12d7b858fe10e6413d8997f736a1886a.jpg",
+        },
+
+        {
+            id:5,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-essen.jpg/33383c8c124ca598579cb6e2a283de23.jpg",
+        },
+
+        {
+            id:6,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-pool.jpg/0427b57fe55a1f30a4bbdff8547eef3c.jpg",
+
+        }
+        ,
+        {
+            id:7,
+            image:"https://www.walde.ch/img/containers/assets/property/l11326/images/8068373-aussicht.jpg/4fe18871a0b672c2f6d1f736ba29d59b.jpg",
+        }
+
+
+
+
+
+
+]
